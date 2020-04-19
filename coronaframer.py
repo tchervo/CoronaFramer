c65743e41ffb517f75d36f0093d12dae4cb2b3e1
tchervo <tchervo@berkeley.edu>
2020-04-18 18:21:24 -0700

    Added support for Google Mobility Data

--- coronaframer.py
+++ coronaframer.py
@@ -10,6 +10,7 @@ import censusdata
 
 nyt_path = os.getcwd() + '/nyt_data/'
 output_data_path = os.getcwd() + '/output_data/'
+google_path = os.getcwd() + '/google_data/'
 log_path = os.getcwd() + '/logs/'
 
 now = datetime.now()
@@ -94,6 +95,63 @@ def get_nyt_data(loc_type='counties', date_filter='now') -> pd.DataFrame:
         is_date = nyt_data['date'] == date_filter
 
         return nyt_data[is_date]
+
+
+def get_google_mobility_data(state: str, county_filter='all', selection_mode='positive') -> pd.DataFrame:
+    """
+    Downloads Google Community Mobility Reports Data (See: https://www.google.com/covid19/mobility/)
+    :param: county_filter: The counties to apply the selection mode to. Default is all which will contain all counties
+    by default
+    :param: selection_mode: Whether to apply positive or negative selection to the counties in county_filter. Default
+    is positive.
+    :param: google_vars: Which variables to apply positive selection to. Default is all which contains all variables
+    for the data set.
+    :return: A data frame containing all the selected variables for all of the counties passing the county filter for a
+    state
+    """
+    file_name = google_path + 'gcmr.csv'
+
+    if os.path.exists(google_path) is not True:
+        os.mkdir(google_path)
+
+    # We need to grab the full report .CSV because all other sources are PDFs
+    md_link = 'https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv'
+    md_request = request.Request(md_link)
+    md_file = request.urlopen(md_request)
+    md_data = md_file.read()
+
+    with open(file_name, 'wb') as file:
+        file.write(md_data)
+
+    # Disable chunking of the CSV file to ensure proper data types across each column
+    md_frame = pd.read_csv(file_name, low_memory=False)
+
+    md_frame = md_frame[md_frame['sub_region_1'] == state]
+
+    if county_filter != 'all':
+        county_filter = [county + ' County' for county in county_filter]
+
+    if selection_mode == 'positive':
+        md_frame = md_frame[md_frame['sub_region_2'].isin(county_filter)]
+    elif selection_mode == 'negative':
+        md_frame = md_frame[~md_frame['sub_region_2'].isin(county_filter)]
+    else:
+        raise ValueError(f'{selection_mode} is not a valid option for selection_mode! Must choose either positive or'
+                         f'negative!')
+
+    # Drop the 'County' suffix from each name to make it line up with the NYT data
+    renamed_counties = [county.replace(' County', '') for county in md_frame['sub_region_2']]
+    md_frame['sub_region_2'] = renamed_counties
+
+    if md_frame[md_frame['date'] == now.strftime('%Y-%m-%d')].empty:
+        newest_date = [date for date in md_frame['date']][-1]
+
+        md_frame = md_frame[md_frame['date'] == newest_date]
+
+    md_frame.drop(columns=['country_region_code', 'country_region', 'date'], inplace=True)
+    md_frame.rename(columns={'sub_region_1': 'state', 'sub_region_2': 'county'}, inplace=True)
+
+    return md_frame
 
 
 def get_fips_for_location(loc_type: str) -> [tuple]:
@@ -206,6 +264,15 @@ def build_frame_for_state(state: str, variables: list, save=True, rename_map={},
     if rename_map != {}:
         state_covid_data.rename(columns=rename_map, inplace=True)
 
+    mobility_data = get_google_mobility_data(state, [county[0] for county in used_counties])
+
+    mobility_data.reset_index(inplace=True)
+    state_covid_data.reset_index(inplace=True)
+
+    state_covid_data = state_covid_data.merge(mobility_data, on='county')
+
+    state_covid_data.drop(columns=['index_x', 'index_y', 'state_y'], inplace=True)
+
     if save:
         state_covid_data.to_csv(output_data_path + f'{state}.csv')
 
@@ -224,10 +291,15 @@ def main():
               '(Ex. Alameda,Contra Costa$Clark,White Pine. Enter state counties in the same order as states.')
         states = input('Please enter the state(s) you are interested in: ')
         counties = input('Please enter the county/counties you are interested in. For all counties, enter all: ')
-        print('For selection mode, enter either "positive" or "negative".\n Positive select will only select the listed'
-              ' counties.\n Negative select will choose everything except the listed counties.\n If entering multiple '
-              'states, separate the selection modes with commas with no spaces (Ex. positive,negative,positive)')
-        selection_mode = input('Please follow the above instructions to input your selection mode: ')
+
+        if counties == 'all':
+            selection_mode = 'positive'
+        else:
+            print('For selection mode, enter either "positive" or "negative".\n Positive select will only select the listed'
+                ' counties.\n Negative select will choose everything except the listed counties.\n If entering multiple '
+                'states, separate the selection modes with commas with no spaces (Ex. positive,negative,positive)')
+            selection_mode = input('Please follow the above instructions to input your selection mode: ')
+
         logger.info(f'Attempting to build frame(s) for {states}')
 
         if ',' in states:
@@ -256,6 +328,15 @@ def main():
 
                     print(f'Finished making frame for {state}')
                     logger.info(f'Finished making frame for {state}')
+        else:
+            state_counties = counties.split(',')
+
+            if state_counties[0].lower() == 'all':
+                build_frame_for_state(states, interest_variables, counties='all',
+                                      selection_mode=selection_mode, rename_map=variable_name_map)
+            else:
+                build_frame_for_state(states, interest_variables, counties=state_counties,
+                                      selection_mode=selection_mode, rename_map=variable_name_map)
 
         run_again = input('Finished output! Would you like to run the program again? (Y/N) ').lower() == 'y'
 
